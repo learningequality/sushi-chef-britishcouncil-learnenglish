fc1b60f2eb8943710d7d67c47eb70487f464af38
Dragon Dave McKee <dragon@scraperwiki.com>
2018-06-26 13:58:16 +0100

    OK

--- crawl.py
+++ crawl.py
@@ -1,26 +1,19 @@
-# british_council
+# crawl redux
 
 import requests
 import requests_cache
-from urllib.parse import urljoin
 from bs4 import BeautifulSoup
-import lxml.html
-
+from html import unescape
 request_headers = {"User-Agent": "Mozilla"}
 
 requests_cache.install_cache()
 import lxml.etree
 
-def get_type(root):
-    is_index = root.xpath("//div[@class='views-row views-row-1 views-row-odd views-row-first']")
-    is_detail = root.xpath("//div[@id='block-i18n-book-navigation-i18n-book-navigation']")
-    if is_index and not is_detail: return Index
-    if is_detail and not is_index: return Detail
-    if is_index and is_detail: raise RuntimeError("Appears to be both index and detail page")
-    if not is_index and not is_detail: raise RuntimeError("Appears to be neither index nor detail page")
-
-class NotRightPageError(Exception):
-    pass
+def get_soup(url, library="html5lib"):
+    r = requests.get(url, headers=request_headers)
+    soup = BeautifulSoup(r.content, library)
+    soup.url = url
+    return soup
 
 class Page(object):
     def __init__(self, url, parent = None):
@@ -31,130 +24,105 @@ class Page(object):
         self.parent = parent
         
     def __repr__(self):
-        return "<{}: {} @ {}>".format(self.title, self.breadcrumb, self.url)
+        return "<{}: {} @ {}>".format(self.title, self.breadcrumb, self.url)        
+    pass
+
+class Index(Page):
+    pass
+
+class Detail(Page):
+    pass
+
+
+# sample pages
+samples = {
+    "http://learnenglish.britishcouncil.org/en/listening": (Index, 1),
+    "http://learnenglish.britishcouncil.org/en/britain-great": (Index, 1),
+    "http://learnenglish.britishcouncil.org/en/beginner-vocabulary": (Index, 1), # note ?page=1 is page 2
+    "http://learnenglish.britishcouncil.org/en/shopping-great/shopping-great-part-1": (Detail, 0),
+    "http://learnenglish.britishcouncil.org/en/english-grammar/pronouns": (Detail, 0), # really?
+}
+
+patterns = {
+    Index: lambda x: x.xpath("//div[@class='views-row views-row-1 views-row-odd views-row-first']"),
+    Detail: lambda x: x.xpath("//div[@id='block-i18n-book-navigation-i18n-book-navigation']"),
+}
+
+def get_type(root):
+    is_index = root.xpath("//div[@class='views-row views-row-1 views-row-odd views-row-first']")
+    is_detail = root.xpath("//div[@id='block-i18n-book-navigation-i18n-book-navigation']")
+    if is_index and not is_detail: return Index
+    if is_detail and not is_index: return Detail
+    if is_index and is_detail: raise RuntimeError("Appears to be both index and detail page")
+    if not is_index and not is_detail: raise RuntimeError("Appears to be neither index nor detail page")
     
-def get_title(soup):
-    h1 = soup.find("h1", {"id": "page-title"})
-    if h1:
-        return h1.text.strip()
-    title = soup.find("title")
-    if title:
-        return title.text.partition("|")[0].strip()
-    raise RuntimeError("No title!")
+def get_rss(root):
+    # does pagination too!
+    # some urls broken -- /en/en/ -> /en/
+    index = 0
+    items = []
+    
+    rss_urls = root.xpath("//a[@class='feed-icon']/@href")
+    if len(rss_urls) == 0:
+        return []
+    while True:
+        xml_soup = get_soup(rss_urls[0]+"?page={}".format(index), "xml")
+        xml_items = xml_soup.findAll("item")
+        for item in xml_items:
+            descr = unescape(item.find("description").text)
+            descr_soup = BeautifulSoup(descr, "html5lib")
+            description_text = descr_soup.getText().strip()
+            
+            items.append({"title": item.find("title").text,
+                          "link": item.find("link").text.replace("/en/en/", "/en/"),
+                          "description": description_text})
+        if xml_items:
+            index = index + 1
+        else:
+            return items
+    
+    """<item>
+        <title>Clothes 2</title>
+        <link>http://learnenglish.britishcouncil.org/en/en/beginner-vocabulary/clothes-2</link>
+        <description>&lt;div class=&quot;field-content&quot;&gt;
+        &lt;p&gt;Do the exercises to learn vocabulary to talk about clothes.&lt;/p&gt;
+        </description>
+       </item>    """
     
     
 
-def get_soup(url):
-    r = requests.get(url, headers=request_headers)
-    soup = BeautifulSoup(r.content, "html5lib")
-    soup.url = url
-    return soup
+def verify_pattern():
+    # dead code, remove later
+    import lxml.html
+    for url, (pattern_type, target_count) in samples.items():
+        r = requests.get(url, headers=request_headers)
+        html = r.content
+        root = lxml.html.fromstring(html)
+        print(lxml.html.tostring(root))
+        for pattern_candidate, verifier in patterns.items():
+            if verifier is None: continue
+            if pattern_type == pattern_candidate:
+                assert verifier(root), "{} should be {} but does not match".format(url, pattern_type)
+            else:
+                assert not verifier(root), "{} matches {} but is a {}".format(url, pattern_candidate, pattern_type)
+                
+        rss = get_rss(root)
+        for item in rss:
+            print (item['link'])
 
-def skill_tree(soup):
-    links = []
-    divs = soup.find_all("div", {"class": "views-field-name"})
-    if not divs:
-        raise NotRightPageError()
-    
-    for d in divs:
-        a = d.find("a", recursive=True)
-        if not a:
-            raise NotRightPageError()
-        url = a.attrs.get('href')
-        if url:
-            links.append(urljoin(soup.url, url))
-    return links
-    
-
-def quick_grammar(soup = get_soup("http://learnenglish.britishcouncil.org/en/quick-grammar")):
-    
-    links = []
-    content = soup.find_all("span", {'class': 'field-content'})
-    for tag in content:
-        link_tags = tag.find_all("a")
-        for tag in link_tags:
-            links.append(urljoin(soup.url, tag.attrs['href']))
-    return links
-
-def get_vocab_nodes(url="http://learnenglish.britishcouncil.org/en/vocabulary-exercises"):
-    urls = []
-    next_page = True
-    while next_page:
-        soup = get_soup(url)
-        try:
-            url_fragment = soup.find("li", {'class': 'pager-next'}).find('a').attrs.get("href") #  None after first page
-            url = urljoin(url, url_fragment)
-        except AttributeError:  # one of the finds is a None.
-            next_page = False
-        spans = soup.find("div", {'id': 'content'}).find_all("span", {'class': 'field-content'})
-        for span in spans:
-            links = span.find_all("a")
-            for link in links:
-                l = urljoin(url, link.attrs.get("href"))
-                if l not in urls:
-                    urls.append(l)
-    return urls
-    
-
-def get_grammar_nodes(base_url = "http://learnenglish.britishcouncil.org/en/english-grammar/"):
-    top_level = ["pronouns", "determiners-and-quantifiers", "possessives", "adjectives", "adverbials", "nouns", "verbs", "clause-phrase-and-sentence"]
-    
-    grammar_nodes = []
-
-    # TODO: also needs structure & actual top-level entrys scraping!
-
-    for item in top_level:
-        soup = get_soup(base_url + item)
-        links = soup.find('aside', {'class': 'sidebars'}).find("ul", {"class": "menu"}).find("ul", {"class": "menu"}).find_all("a", {"class": "menu__link"})
+print ("Success!")
         
-        for link in links:
-            grammar_nodes.append(urljoin(base_url, link.attrs['href']))
-    return grammar_nodes
-
-#print(quick_grammar())
-
-
-
-pages = set([Page("http://learnenglish.britishcouncil.org/en/skills")])
-deadurls = set()
-
-def url_filter(url):
-    if url in deadurls:
-        return False
-    if "britishcouncil" not in url:
-        print (url)
-        return False
-    return True
-
-
-def target_page(page):
-    article = page.find("article")
-    
-       
-    
-
-while pages:
-    page = pages.pop()
-    print(page.url)
-    deadurls.add(page.url)
-    try:
-        urls = skill_tree(page.soup)
-        print ("Skill tree {}".format(len(urls)))
-        for url in urls:
-            if url_filter(url):
-                pages.add(Page(url, parent=page.url))
-        continue
-    except NotRightPageError:
-        print ("Not Skill Tree")
-        pass
-    try:
-        urls = set(quick_grammar(page.soup))
-        print ("Quick Grammar {}".format(len(urls)))
-        print (urls)
-        for url in urls:
-            if url_filter(url):
-                pages.add(Page(url, parent=page.url))
-        continue
+            
         
-    except NotRightPageError:
-        print ("not quick grammar")
+        
+    
+verify_pattern()
+    
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
--- indiv.py
+++ indiv.py
@@ -1,45 +1,44 @@
-# individual
-
 import requests
 import requests_cache
+from urllib.parse import urljoin
 from bs4 import BeautifulSoup
-from lxml import etree
 
-from ricecooker.classes import questions
-from ricecooker.classes.nodes import ExerciseNode
-from ricecooker.classes.licenses import get_license
-from le_utils.constants import licenses, exercises
-from le_utils.constants.languages import getlang
-from ricecooker.classes.questions import PerseusQuestion
-
-requests_cache.install_cache()
 request_headers = {"User-Agent": "Mozilla"}
 
+requests_cache.install_cache()
+import lxml.etree
+
+
+def individual(url = "https://learnenglish.britishcouncil.org/en/youre-hired/episode-03"):
+    html = requests.get(url, headers=request_headers)
+    soup = BeautifulSoup(html.text, "html5lib")
+    article = soup.find("article")
     
+    # delete the comments
+    comments = article.find_all("section", {"id": "comments"})
+    [s.extract() for s in comments]
+    
+    # unhide hidden styles
+    divs = article.find_all("div", {"style": "display: none;"})
+    for div in divs:
+        div.attrs['style'] = ""
+    
+    # replace viddler videos
+    # see viddler_demo for the origin of the URL which may not be viable in all cases.
+    # we might need to use the API each time.
+    
+    viddlers = article.find_all("div", {"class":"viddler-auto-embed"})
+    
+    for vid in viddlers:
+        viddler_id = vid.attrs['data-video-id'] 
+        video = BeautifulSoup('<video controls><source src="http://www.viddler.com/file/{}/html5" type="video/mp4"></video>'.format(viddler_id), "html5lib")
+        vid.replaceWith(video)
+    
+    return article
+        
+def output_html(soup):
+    with open("output.html", "w")  as f:
+        f.write(soup.prettify())
+    print (soup.prettify())
 
-def get_individual_page(url):
-    r = requests.get(url, headers = request_headers)
-    soup = BeautifulSoup(r.content, "html5lib") # html5lib handles encodings, so throw raw bytes at it with .content
-    body = (soup.find("article").find("div", {"class": "field-name-body"}))
-    xml_elements = (body.find_all("a", {"class": "embed"}))
-    xmls = [a.attrs['href'].strip() for a in xml_elements]
-    return xmls
-
-
-def handle_xml(top_xml_url):
-    r = requests.get(top_xml_url, headers=request_headers)
-    soup = BeautifulSoup(r.text, "xml")  # note: use .text to decode UTF-8 properly rather than just having bytes
-    return soup
-    #print(soup.prettify())
-
-if __name__ == "__main__":
-    print (do_it())
-
-
-
-
-
-
-
-
-
+output_html(individual() )
