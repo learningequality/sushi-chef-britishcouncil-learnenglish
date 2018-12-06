4a812cf775bd447cbf2b4e896774721a8241e69c
Dragon Dave McKee <dragon@scraperwiki.com>
2018-12-06 18:23:09 +0000

    Demo writing

--- writing_index.py
+++ writing_index.py
@@ -11,23 +11,11 @@ pager_element: 9
 field_section_tid[2393]: 2393
 field_section_tid[2554]: 2554
 page: 0,0,0,0,0,0,0,0,0,3
-ajax_page_state[jquery_version]: 1.10"""
+"""
 
-headers = """Accept: application/json, text/javascript, */*; q=0.01
-Accept-Encoding: gzip, deflate
-Accept-Language: en-GB,en-US;q=0.9,en;q=0.8
-Cache-Control: no-cache
-Connection: keep-alive
-Content-Length: 9842
-Content-Type: application/x-www-form-urlencoded; charset=UTF-8
-Cookie: has_js=1; learnenglish-britishcouncil-org-eu-cookie=1
-DNT: 1
-Host: learnenglish.britishcouncil.org
-Origin: http://learnenglish.britishcouncil.org
-Pragma: no-cache
-Referer: http://learnenglish.britishcouncil.org/en/writing
-User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/70.0.3538.77 Chrome/70.0.3538.77 Safari/537.36
-X-Requested-With: XMLHttpRequest"""
+headers = """
+User-Agent: Learning-Equality
+"""
 
 data = dict([[h.partition(': ')[0], h.partition(': ')[2]] for h in datastr.strip().split('\n')])
 head = dict([[h.partition(': ')[0], h.partition(': ')[2]] for h in headers.strip().split('\n')])
